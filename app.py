1f6903c43e01ba65c7239227a37b912356b9625f
Hayssam Noweir <hayssam.noweir@rackspace.com>
2023-02-23 11:40:22 +0100

    updt

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request
-
+import requests
 app = Flask(__name__)
 
 @app.route("/")
@@ -11,3 +11,11 @@ def index():
     return f"<h3>All Headers</h3> <div>{headers_str} </div>"
 
 
+@app.route("/hello")
+def hello():
+    headers: dict=request.headers
+    if "X-Ms-Client-Principal-Name" not in headers:
+        return "Sorry, you are not logged in, please <a href=\".auth/me\">login</a>"
+    
+    req=requests.get('.auth/me')
+    return req.body
