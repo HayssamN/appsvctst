69a424c839cc0942b232762aa3d6d86ce95a74dd
Hayssam Noweir <hayssam.noweir@rackspace.com>
2023-02-23 13:38:38 +0100

    updt

--- app.py
+++ app.py
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def index():
     headers: dict=request.headers
     headers_str:str=''
+    cookies_str:str=''
     for k,v in headers.items():
         headers_str+=f"<p><b>{k}:</b> {v}</p>"
-    return f"<h3>All Headers</h3> <div>{headers_str} </div>"
+    for k,v in request.cookies.items():
+        cookies_str+=f"<p><b>{k}:</b> {v}</p>"
+    return f"<h3>Cookies</h3> <div>{cookies_str} </div>"
 
 
 @app.route("/hello")
@@ -16,6 +19,7 @@ def hello():
     headers: dict=request.headers
     if "X-Ms-Client-Principal-Name" not in headers:
         return "Sorry, you are not logged in, please <a href=\".auth/login/aad\">login</a>"
-    
+
+    userEmail=headers["X-Ms-Client-Principal-Name"]
     req=requests.get(f'https://{headers["Disguised-Host"]}/.auth/me')
-    return "<div><p><a href=\".auth/logout\">Logout</a></p></div>" + req.text
+    return f"<div><p>Hi {userEmail}, <a href=\".auth/logout\">Logout</a></p></div>" + req.text
