a52e3294a31ef69432eaeb92ab3cf313b1255ddc
Hayssam Noweir <hayssam.noweir@rackspace.com>
2023-03-13 09:02:24 +0000

    Add wbhk

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ def hello():
 
 @app.route('/wbhk', methods=['POST'])
 def wbhk():
-    return f'<h2>Headers</h2><pre>{request.headers}</pre><p>--</p><h2>Payload</h2><pre>{json.dumps(request.get_json(), indent=2)}</pre>'
+    return "OK"
+    print( f'<h2>Headers</h2><pre>{request.headers}</pre><p>--</p><h2>Payload</h2><pre>{json.dumps(request.get_json(), indent=2)}</pre>')
 
 
     userEmail=headers["X-Ms-Client-Principal-Name"]
